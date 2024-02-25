7d5ba2fdbb32cd62c76947473d2cfdd5c7eb8312
haprcr <124026578+haprcr@users.noreply.github.com>
2024-02-25 13:52:41 +0530

    Create 3.sql

---
+++ 25_feb/3.sql
@@ -0,0 +1,13 @@
+CREATE TABLE 
+`cme-poc-364409.cud_resource_metrics.cud_resource_metric_2_3_23feb`
+(
+  Invoice_Month STRING,
+  Service STRING,
+  Commitment_SKU STRING,
+  Commitment_Cost FLOAT64,
+  Commitment_usage_amount FLOAT64,
+  Resource_Usage_amount FLOAT64,
+  Total_Resource_Cost FLOAT64,
+  CUD_Utilisation FLOAT64
+);
+
